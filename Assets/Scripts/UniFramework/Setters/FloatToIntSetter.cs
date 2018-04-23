71245ef0b530d5f3d8ee6e2b65b74ff8f36b2ad3
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-04-23 15:44:35 -0300

    Setters: IntToFloat, FloatToInt

---
+++ Assets/Scripts/UniFramework/Setters/FloatToIntSetter.cs
@@ -0,0 +1,18 @@
+﻿namespace UniFramework.Setters
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+
+    [AddComponentMenu("Setter/Variables/FloatToInt Setter")]
+    public class FloatToIntSetter : Setter
+    {
+
+        public FloatReference newValue;
+        public IntVariable variable;
+
+        public override void Set()
+        {
+            variable.SetValue((int)newValue.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/UniFramework/Setters/IntToFloatSetter.cs
@@ -0,0 +1,18 @@
+﻿namespace UniFramework.Setters
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+
+    [AddComponentMenu("Setter/Variables/IntToFloat Setter")]
+    public class IntToFloatSetter : Setter
+    {
+
+        public IntReference newValue;
+        public FloatVariable variable;
+
+        public override void Set()
+        {
+            variable.SetValue(newValue.Value);
+        }
+    }
+}
