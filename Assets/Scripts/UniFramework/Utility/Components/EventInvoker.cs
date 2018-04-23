ed113a07f49f58dd4e1adcc6bb6121e7b802bb36
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-04-23 15:34:31 -0300

    EventInvoker

---
+++ Assets/Scripts/UniFramework/Utility/Components/EventInvoker.cs
@@ -0,0 +1,38 @@
+﻿namespace UniFramework.Utility
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class EventInvoker : MonoBehaviour
+    {
+        public UnityEvent unityEvent;
+
+        public bool invokeOnAwake;
+        public bool invokeOnStart;
+        public bool invokeOnUpdate;
+
+        void Awake()
+        {
+            if (invokeOnAwake)
+            {
+                unityEvent.Invoke();
+            }
+        }
+
+        void Start()
+        {
+            if (invokeOnStart)
+            {
+                unityEvent.Invoke();
+            }
+        }
+
+        void Update()
+        {
+            if (invokeOnUpdate)
+            {
+                unityEvent.Invoke();
+            }
+        }
+    }
+}
