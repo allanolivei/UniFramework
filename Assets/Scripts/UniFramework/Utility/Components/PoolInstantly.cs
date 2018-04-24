b123833dd057d1b12fd5d0719a5cad6871336af3
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-04-24 16:58:32 -0300

    Fixed Pool components, both with new names and structure

---
+++ Assets/Scripts/UniFramework/Utility/Components/PoolInstantly.cs
@@ -0,0 +1,106 @@
+﻿namespace UniFramework.Utility
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class PoolInstantly : MonoBehaviour
+    {
+        public BoolReference poolOnAwake;
+        [Tooltip("Warning: if poolOnAdditiveScene is enabled, the pooling on Start will ONLY work if the scene is loaded using Additive mode. Awake and OnEnable will work on Additive and Single (Default).")]
+        public BoolReference poolOnStart;
+        public BoolReference poolOnEnable;
+        [Space(10)]
+        public GameObject prefab;
+        public StringReference poolID;
+        public IntReference amountToPool;
+        [Tooltip("Warning: enabling this will make pooling on Start ONLY work if the scene is loaded using Additive mode. Awake and OnEnable work both ways.")]
+        public BoolReference poolOnAdditiveScene;
+        [Tooltip("You may leave this empty if Pool On Additive Scene is set to false")]
+        public StringReference targetSceneName;
+
+        void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingAwake;
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingStart;
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingEnable;
+        }
+
+        void Awake()
+        {
+            if (poolOnAwake)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingAwake;
+                else
+                    Pool();
+            }
+        }
+
+        void Start()
+        {
+            if (poolOnStart)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingStart;
+                else
+                    Pool();
+            }
+        }
+
+        void OnEnable()
+        {
+            if (poolOnEnable)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingEnable;
+                else
+                    Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingAwake(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingStart(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingEnable(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        /// <summary>
+        /// Intantiate and pool instances of a prefab acording to the settings of the SpawnFromPool's instance variables
+        /// </summary>
+        public void Pool()
+        {
+            Pool(prefab, poolID, amount: amountToPool);
+        }
+
+        /// <summary>
+        /// Intantiate and pool instances of a prefab
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <param name="id">The id registered to the prefab's ObjectPool</param>
+        /// <param name="amount"></param>
+        public static void Pool(GameObject prefab, string id, int amount = 1)
+        {
+            ObjectPool.Register(id, prefab);
+            ObjectPool.Warm(prefab, id, amount);
+        }
+    }
+}
---
+++ Assets/Scripts/UniFramework/Utility/Components/PoolOverTime.cs
@@ -0,0 +1,125 @@
+﻿namespace UniFramework.Utility
+{
+    using System.Collections;
+    using UniFramework.Variables;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class PoolOverTime : MonoBehaviour
+    {
+        public BoolReference poolOnAwake;
+        [Tooltip("Warning: if poolOnAdditiveScene is enabled, the pooling on Start will ONLY work if the scene is loaded using Additive mode. Awake and OnEnable will work on Additive and Single (Default).")]
+        public BoolReference poolOnStart;
+        public BoolReference poolOnEnable;
+        [Space(10)]
+        public GameObject prefab;
+        public StringReference poolID;
+        [Tooltip("The name of the scene where this prefab will be pooled")]
+        public StringReference targetSceneName;
+        [Tooltip("Warning: enabling this will make pooling on Start ONLY work if the scene is loaded using Additive mode. Awake and OnEnable will work both ways.")]
+        public BoolReference poolOnAdditiveScene;
+        [Space(10)]
+        public IntReference amountPerTick;
+        public IntReference ticks;
+        public FloatReference interval;
+        public FloatReference delay;
+
+        void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingAwake;
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingStart;
+            SceneManager.activeSceneChanged -= OnLevelFinishedLoadingEnable;
+        }
+
+        void Awake()
+        {
+            if (poolOnAwake)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingAwake;
+                else
+                    Pool();
+            }
+        }
+
+        void Start()
+        {
+            if (poolOnStart)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingStart;
+                else
+                    Pool();
+            }
+        }
+
+        void OnEnable()
+        {
+            if (poolOnEnable)
+            {
+                if (poolOnAdditiveScene)
+                    SceneManager.activeSceneChanged += OnLevelFinishedLoadingEnable;
+                else
+                    Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingAwake(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingStart(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        void OnLevelFinishedLoadingEnable(Scene previousScene, Scene newScene)
+        {
+            if (newScene.name == targetSceneName)
+            {
+                Pool();
+            }
+        }
+
+        /// <summary>
+        /// Spawn and pool over time instances of a prefab acording to the settings of the SpawnFromPool's instance variables
+        /// </summary>
+        public void Pool()
+        {
+            Pool(prefab, poolID, interval, ticks, amountPerTick, delay: delay);
+        }
+
+        /// <summary>
+        /// Spawn and pool over time instances of a prefab
+        /// </summary>
+        /// <param name="prefab">GameObject (Prefab) to be spawned</param>
+        /// <param name="id">The id registered to the prefab's ObjectPool</param>
+        /// <param name="interval">Interval between spawnings</param>
+        /// <param name="ticks">Amount of spawnings</param>
+        /// <param name="spawnAmountPerTick">Amount to be spawned on every interval</param>
+        /// <param name="delay">Delay before the first tick</param>
+        public void Pool(GameObject prefab, string id, float interval, int ticks, int spawnAmountPerTick, float delay = 0)
+        {
+            ObjectPool.Register(id, prefab);
+            StartCoroutine(WarmOverTime(id, interval, ticks, spawnAmountPerTick, delay));
+        }
+
+        IEnumerator WarmOverTime(string id, float interval, int ticks, int spawnAmountPerTick, float delay = 0)
+        {
+            yield return new WaitForSeconds(delay);
+            WaitForSeconds wait = new WaitForSeconds(interval);
+            for (int i = 0; i < ticks; i++)
+            {
+                ObjectPool.Warm(prefab, id, spawnAmountPerTick);
+                yield return wait;
+            }
+        }
+    }
+}
