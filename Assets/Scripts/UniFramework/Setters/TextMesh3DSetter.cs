88a5f8dc205bece61d9e0d41c07a6aa71d3711b2
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-04-24 10:07:10 -0300

    Added prefix, suffix and addValue to TextMeshSetters

--- Assets/Scripts/UniFramework/Setters/TextMesh3DSetter.cs
+++ Assets/Scripts/UniFramework/Setters/TextMesh3DSetter.cs
@@ -11,22 +11,30 @@
         public ScriptableVariable variable;
         public TextMeshPro text;
         public string format;
+        public string prefix;
+        public string suffix;
+        public float valueToAddIfIntOrFloat;
 
         public override void Set()
         {
             if (variable.GetType().ToString() != "StringVariable")
             {
+                var varContent = variable.DynamicGet();
+                if (varContent is int || varContent is float)
+                {
+                    varContent += valueToAddIfIntOrFloat;
+                }
                 try
                 {
-                    text.SetText(variable.DynamicGet().ToString(format));
+                    text.SetText($"{prefix}{varContent.ToString(format)}{suffix}");
                 }
                 catch (System.Exception)
                 {
-                    text.SetText(variable.DynamicGet().ToString());
+                    text.SetText($"{prefix}{varContent.ToString()}{suffix}");
                 }
             }
             else
-                text.SetText(variable.DynamicGet());
+                text.SetText($"{prefix}{variable.DynamicGet()}{suffix}");
         }
     }
 }
