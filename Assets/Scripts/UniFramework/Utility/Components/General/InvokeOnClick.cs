73cad0a47929e43dde0e4a9419dab222a93a9632
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-05-18 09:34:17 -0300

    InvokeOnClick

---
+++ Assets/Scripts/UniFramework/Utility/Components/General/InvokeOnClick.cs
@@ -0,0 +1,47 @@
+﻿namespace UniFramework.Utility
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class InvokeOnClick : MonoBehaviour
+    {
+        public BoolReference setCursorPos;
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.ShowIf("ShowVariable")]
+#endif
+        public Vector2Variable cursorPos;
+        public UnityEvent OnClick;
+
+        private Camera cam;
+        private Vector2 mousePos;
+
+        private void Awake()
+        {
+            cam = Camera.main;
+        }
+
+        private void SetPos()
+        {
+            if (setCursorPos)
+            {
+                mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+                cursorPos.Value = mousePos;
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            SetPos();
+            OnClick.Invoke();
+        }
+
+#if ODIN_INSPECTOR
+        private bool ShowVariable()
+        {
+            return setCursorPos.Value;
+        }
+#endif
+    }
+}
