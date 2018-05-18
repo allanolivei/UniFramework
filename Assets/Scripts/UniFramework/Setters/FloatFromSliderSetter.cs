66aeca4319a7a631a70cd7693e578112b31816ba
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-05-18 09:33:35 -0300

    Float/Int From Slider Setters

---
+++ Assets/Scripts/UniFramework/Setters/FloatFromSliderSetter.cs
@@ -0,0 +1,18 @@
+﻿namespace UniFramework.Setters
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [AddComponentMenu("Setter/Variables/Float From Slider Setter")]
+    public class FloatFromSliderSetter : Setter
+    {
+        public Slider slider;
+        public FloatVariable variable;
+
+        public override void Set()
+        {
+            variable.Value = slider.value;
+        }
+    }
+}
---
+++ Assets/Scripts/UniFramework/Setters/IntFromSliderSetter.cs
@@ -0,0 +1,18 @@
+﻿namespace UniFramework.Setters
+{
+    using UniFramework.Variables;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [AddComponentMenu("Setter/Variables/Int From Slider Setter")]
+    public class IntFromSliderSetter : Setter
+    {
+        public Slider slider;
+        public IntVariable variable;
+
+        public override void Set()
+        {
+            variable.Value = (int)slider.value;
+        }
+    }
+}
